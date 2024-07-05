3c16455d52a1f4d00b98d7f41ef9bd4b0ec2d405
Star922 <kyryllnetesa@gmail.com>
2024-07-06 00:54:35 +0530

    added mongo

--- create_database.py
+++ create_database.py
@@ -1,35 +1,39 @@
-import sqlite3
-
+from pymongo import MongoClient
+from datetime import datetime
 def setup_userdb():
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-    # Create users table
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        username TEXT PRIMARY KEY,
-        password TEXT NOT NULL,
-        email TEXT NOT NULL,
-        searchRetries INTEGER NOT NULL,
-        summaryRetries INTEGER NOT NULL
-    );
-    ''')
-    conn.commit()
-    conn.close()
+    client=MongoClient(st.secrets['mongo']['url'])
+    db=client['user_database']
+    
+    if 'users' not in db.list_collection_names():
+        db.create_collection('users', validator={
+                 '$jsonSchema': {
+                     'bsonType': 'object',
+                     'required': ['username', 'password', 'email'],
+                     'properties': {
+                         'username': {'bsonType': 'string'},
+                         'password': {'bsonType': 'string'},
+                         'email': {'bsonType': 'string'},
+                         'searchRetries': {'bsonType': 'int'},
+                         'summaryRetries': {'bsonType': 'int'},
+                         'created_at':{'bsonType':'date'}
+                     }
+                 }
+             })
+        db['users'].create_index([('username',1)],unique=True)
 
 def setup_commdb():
-    conn = sqlite3.connect('users.db')
-    c = conn.cursor()
-    # Create users table
-    c.execute('''
-    CREATE TABLE IF NOT EXISTS comments (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT NOT NULL,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        comment TEXT NOT NULL
-    );
-    ''')
-    conn.commit()
-    conn.close()
+    client=MongoClient('mongodb+srv://sandipt335:4q0WU7BW9kW5uXkX@cluster0.mm9pw6p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',server_api=ServerApi('1'))
+    db = client['user_database']
+    if 'comments' not in db.list_collection_names():
+        db.create_collection('comments', validator={
+            '$jsonSchema': {
+                'bsonType': 'object',
+                'required': ['username', 'comment'],
+                'properties': {
+                    'username': {'bsonType': 'string'},
+                    'comment': {'bsonType': 'string'},
+                    'created_at': {'bsonType': 'date'}
+                }
+            }
+        })
 
-if __name__=='__main__':
-    setup_commdb()
